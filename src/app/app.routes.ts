12e0989b367e2a9814c1f7b16e51ef5b1643f654
Legr59 <legranmar@gmail.com>
2026-06-26 11:04:07 +0200

    Initial Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
-import { Home } from './home/home';
-import { Education } from './education/education';
-import { Professional } from './professional/professional';
-import { Projects } from './projects/projects';
-import { Activities } from './activities/activities';
-import { Mobility } from './mobility/mobility';
-import { Contact } from './contact/contact';
+import { Home } from './components/home/home';
+import { Education } from './components/education/education';
+import { Professional } from './components/professional/professional';
+import { Projects } from './components/projects/projects';
+import { Activities } from './components/activities/activities';
+import { Mobility } from './components/mobility/mobility';
+import { Contact } from './components/contact/contact';
 
 export const routes: Routes = [
   { path: '', component: Home },
